fa836e265b790f1b5d7ff8e2022781b8acf5ee50
Zubaer Alam Bihim <zubaerbihim@gmail.com>
2021-04-29 17:59:13 +0600

    Tested something and added navigation drawer only

--- app/src/main/java/com/indiancosmeticsbd/app/Views/MainActivity.java
+++ app/src/main/java/com/indiancosmeticsbd/app/Views/MainActivity.java
@@ -1,14 +1,21 @@
-package com.indiancosmeticsbd.app;
+package com.indiancosmeticsbd.app.Views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.appbar.MaterialToolbar;
+import com.indiancosmeticsbd.app.R;
+
 public class MainActivity extends AppCompatActivity {
+
+    MaterialToolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.toolbar_main);
+        setSupportActionBar(toolbar);
     }
 }
